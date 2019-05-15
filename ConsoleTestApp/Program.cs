1eb77f751fa565a9b08f41a7eb333b28de9a08ce
gsharm2020 <46314053+gsharm2020@users.noreply.github.com>
2019-05-15 12:30:48 +0530

    updated back to 15.

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -61,7 +61,7 @@ namespace ConsoleTestApp
             Trigonometric.TrianlgeSidesInput(12, 12, 12);
             Console.ReadLine();
 
-            Trigonometric.TrianlgeSidesInput(12, 12, 12);
+            Trigonometric.TrianlgeSidesInput(12, 12, 15);
             Console.ReadLine();
 
             Trigonometric.TrianlgeSidesInput(12, 20, 25);
