edd13a658baaf283e4487d198a0c50732f26af83
gaurav sharma <gaurav.sharma117@infosys.com>
2019-05-15 11:36:05 +0530

    First Comment

---
+++ ConsoleTestApp.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleTestApp.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //First  
+            Trigonometric.TrianlgeSidesInput(10, 10, 15);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Second Test  
+            Trigonometric.TrianlgeSidesInput(10.5, 10.5, 15);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //Thisrd Test  
+            Trigonometric.TrianlgeSidesInput(10.5, 12.5, 15.5);
+
+        }
+    }
+}
